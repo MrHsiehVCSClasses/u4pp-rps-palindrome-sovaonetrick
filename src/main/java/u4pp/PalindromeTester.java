1c821f173dfaa09e8a92a62d977721745fc6ea90
sovaonetrick <iankim8923@gmail.com>
2023-10-27 07:04:32 -0700

    lets gooo

--- src/main/java/u4pp/PalindromeTester.java
+++ src/main/java/u4pp/PalindromeTester.java
@@ -10,6 +10,50 @@ public class PalindromeTester {
      */
     public static void execute(Scanner sc) {
         /* YOUR CODE HERE */
+        Scanner pickles = new Scanner(System.in);
+        Boolean keepTesting = true;
+
+        while (keepTesting == true) {
+
+        System.out.println("Welcome to Palindrome Tester");
+        System.out.println("Enter a phrase: ");
+        String piccatini = pickles.nextLine();
+
+        Boolean apple = isPalindrome(piccatini);
+        
+        if (apple == true) {
+            System.out.println(piccatini + " is a palindrome");
+        }
+        else {
+            if (apple == false) {
+                System.out.println(piccatini + " is not a palindrome");
+        }
+        
+        String avocado = "p";
+        String yes = "y";
+        String no = "n";
+
+        while (true) {
+
+        System.out.println("Keep testing? (Y)es or (N)o: ");
+        avocado = pickles.nextLine();
+        avocado = avocado.toLowerCase();
+        
+        if (avocado.equals(yes)) {
+            break;
+        }
+        else {
+            if (avocado.equals(no)) {
+                keepTesting = false;
+            }
+            else {
+                System.out.println("Invalid input, please try again");
+            }
+        }
+
+        }
+    }
+        pickles.close(); }
     }
 
     /**
@@ -17,6 +61,27 @@ public class PalindromeTester {
      */
     public static boolean isPalindrome(String phrase) {
         /* YOUR CODE HERE */
+        String reversed = "";
+        String letter;
+        String numbas = "\\d";
+        String punctus = "[\\p{Punct}]";
+
+        phrase = phrase.toLowerCase();
+        phrase = phrase.replaceAll(numbas, "");
+        phrase = phrase.replace(" ", "");
+        phrase = phrase.replaceAll(punctus, "");
+
+        for (int i = 0; i < phrase.length(); i++) {
+            letter = phrase.substring(i, i + 1);
+            reversed = letter + reversed;
+        }
+
+        if (reversed.equals(phrase)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /* Any helper methods you might need */
--- src/main/java/u4pp/RockPaperScissors.java
+++ src/main/java/u4pp/RockPaperScissors.java
@@ -1,7 +1,7 @@
 package u4pp;
 
 /**
-* JAVADOC HERE
+* @author is going insane and cannot look at this screen anymore. Code lets you play rock paper scissors but is inconvenient and you should google a rock paper scissors game instead
 */
 import java.util.Scanner;
 
@@ -10,17 +10,123 @@ public class RockPaperScissors {
     /* any code you need here for part b */
 
     /**
-     * JAVADOC HERE
+     * picks a random number from 0 to 1 with about equal 1/3 chances for r, p, s
      */
+    public static String genChoice() {
+        double gen;
+        String rocka = "r";
+        String papa = "p";
+        String sza = "s";
+
+        gen = Math.random();
+        if (gen >= 0.67) {
+            return rocka;
+        }
+        else {
+            if (gen <= 0.33) {
+                return papa;
+            }
+            else {
+                return sza;
+            }
+        }
+    }
     public static void play(Scanner sc) {
         /* PART B: YOUR CODE HERE */
+        int wins = 0;
+        int ties = 0;
+        int losses = 0;
+        Boolean keepPlaying = true;
+
+        Scanner playCheck = new Scanner(System.in);
+
+        sc = new Scanner(System.in);
+
+        while (keepPlaying == true) {
+        
+        System.out.print("Please choose either R for Rock, P for Paper, S for Scissors, or A for any:");
+        String player = sc.nextLine();
+
+        if ("a" == player.toLowerCase()) {
+            player = genChoice();
+        }
+
+        String computer = genChoice();
+
+        int result = results(player, computer);
+
+        if (result == 0) {
+            ties++;
+            System.out.println("It's a tie!");
+        }
+        else {
+            if (result == 1) {
+                wins++;
+                System.out.println("You win!");
+            }
+            else {
+                if (result == -1) {
+                    losses++;
+                    System.out.println("You lose.");
+                }
+            }
+        }
+        System.out.println("You have " + wins + " wins and " + losses + " losses and " + ties + " ties.");
+        
+        String decision = "p";
+        Boolean scuffedCheck = true;
+
+        while (scuffedCheck == true) {
+
+        System.out.print("Would you like to play again? (Y)es or (N)o: ");
+        decision = playCheck.nextLine();
+        decision = decision.toLowerCase();
+
+        String optionY = "y";
+        String optionN = "n";
+
+        if (decision.equals(optionY) == true) {
+            keepPlaying = true;
+            scuffedCheck = false;
+        }
+        else {
+            if (decision.equals(optionN)) {
+            keepPlaying = false;
+            scuffedCheck = false;
+            }
+            else {
+                System.out.println("Invalid Input, please try again");
+            }
+        }
+        }
     }
+    sc.close();
+    playCheck.close();
+
+    }
+    
+
+    
+    
+    
 
     /**
-     * JAVADOC HERE
+     * basic "who wins and who loses" code following irl rules
      */
     public static int results(String playerChoice, String computerChoice) {
-        /* PART A: YOUR CODE HERE */
+        playerChoice = playerChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+
+        if (playerChoice.equals(computerChoice)) {
+            return 0;
+        }
+
+        if (playerChoice.equals("r") && computerChoice.equals("s") || playerChoice.equals("s") && computerChoice.equals("p") || playerChoice.equals("p") && computerChoice.equals("r")) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
     }
 
     /* 
